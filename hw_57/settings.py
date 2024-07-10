31046e92ac1101c333d4dc2c23f2e3bd90a1d8f2
Korenchuk Rustam <smohghtxx@gmail.com>
2024-07-10 21:54:35 +0600

    добавил вебапп, подключил постгресс

--- hw_57/settings.py
+++ hw_57/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "webapp"
 ]
 
 MIDDLEWARE = [
@@ -76,10 +77,15 @@ WSGI_APPLICATION = 'hw_57.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "trakcer",
+        'USER': 'user',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
+
 
 
 # Password validation
