a0550f02ea742b5aa55de2ee5fabc7b2a0853179
Korenchuk Rustam <smohghtxx@gmail.com>
2024-07-10 23:22:59 +0600

    создали модели, сделали миграции, применили, зареги модели в админке,

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Status, Type, Issue
 
-# Register your models here.
+
+admin.site.register(Status)
+admin.site.register(Type)
+admin.site.register(Issue)
--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Status(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"Статус: {self.name}"
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"Тип: {self.name}"
+
+
+class Issue(models.Model):
+    summary = models.CharField(max_length=150, verbose_name="Заголовок")
+    description = models.TextField(blank=True, null=True)
+    status = models.ForeignKey(Status, on_delete=models.PROTECT)
+    type = models.ForeignKey(Type, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Заголовок: {self.summary}"
