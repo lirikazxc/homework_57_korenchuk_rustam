ab417472293447d4a627d29dc18c9caef486c3c1
Korenchuk Rustam <smohghtxx@gmail.com>
2024-07-29 11:57:32 +0600

    добавил поле юзер (м2м) в модели проекта, применил миграцию, привязал все текущие проекты к юзеру админа, после сделал поле юзера в модели проекта обязательным

--- webapp/models/project.py
+++ webapp/models/project.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -6,6 +7,7 @@ class Project(models.Model):
     end_date = models.DateField(null=True, blank=True)
     name = models.CharField(max_length=100)
     description = models.TextField()
+    users = models.ManyToManyField(User, related_name='projects', blank=False, null=False)
 
     def __str__(self):
         return self.name
